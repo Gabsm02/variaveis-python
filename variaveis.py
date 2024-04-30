3ca7eb8a8ba97cd4480b3cf4526e4e5bb4a3f40a
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-04-30 14:08:28 -0300

    Aprendendo sobre variaveis

---
+++ variaveis.py
@@ -0,0 +1,24 @@
+# Variáveis de texto
+Nome = "Gabriel"
+print(Nome)
+
+variavel_com_espaco = "Variável com espaço"
+print(variavel_com_espaco)
+
+# Variáveis numéricas inteiras
+
+idade = int(22)
+
+# Variáveis numéricas flutuantes
+altura = float(1.80)
+
+# Variaveis booleanas
+is_True = True
+is_False = False
+
+# Utilizar nomes de variáveis que façam sentido
+
+
+
+
+
